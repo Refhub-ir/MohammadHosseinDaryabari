6c54efbcd6425ec138bf85b6e17ce098fc3d2936
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 21:02:30 +0330

    Configure BookKeywordConfiguration

---
+++ Refhub/Data/Configuration/BookKeywordConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Models;
+
+public class BookKeywordConfiguration : IEntityTypeConfiguration<BookKeyword>
+{
+    public void Configure(EntityTypeBuilder<BookKeyword> builder)
+    {
+        builder.HasKey(bk => new { bk.BookId, bk.KeywordId });
+
+        builder.HasOne(bk => bk.Book)
+               .WithMany(b => b.BookKeywords)
+               .HasForeignKey(bk => bk.BookId);
+
+        builder.HasOne(bk => bk.Keyword)
+               .WithMany(k => k.BookKeywords)
+               .HasForeignKey(bk => bk.KeywordId);
+    }
+}
