dce794eb72b5c3b1c4b4dad42ebcb219500bf162
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 21:03:25 +0330

    configure CategoryConfiguration

---
+++ Refhub/Data/Configuration/BookRelationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Models;
+
+public class BookRelationConfiguration : IEntityTypeConfiguration<BookRelation>
+{
+    public void Configure(EntityTypeBuilder<BookRelation> builder)
+    {
+        builder.HasKey(br => new { br.BookId, br.RelatedBookId });
+
+        builder.HasOne(br => br.Book)
+               .WithMany(b => b.RelatedTo)
+               .HasForeignKey(br => br.BookId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(br => br.RelatedBook)
+               .WithMany(b => b.RelatedFrom)
+               .HasForeignKey(br => br.RelatedBookId)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Refhub/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Models;
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasMany(c => c.Books)
+            .WithOne(c => c.Category)
+            .HasForeignKey(c => c.CategoryId);
+    }
+}
