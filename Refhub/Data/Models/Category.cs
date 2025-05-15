91e319601d2ed2653f2bcd224b2bc95a739f32ef
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 20:59:48 +0330

    add Category model

---
+++ Refhub/Data/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Refhub.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
