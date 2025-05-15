7d23328e03fcf083895440091d5126bc537efc7d
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 21:02:13 +0330

    configure BookConfiguration

---
+++ Refhub/Data/Configuration/BookConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Models;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.HasKey(x => x.Id);
+       
+        builder.HasOne(b => b.Category)
+               .WithMany()
+               .HasForeignKey(b => b.CategoryId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
