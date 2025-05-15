3654b89a67a503d4dbe32824d830c67cf9cee306
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 21:01:20 +0330

    configure AuthorConfiguration

---
+++ Refhub/Data/Configuration/AuthorConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Models;
+
+public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+{
+    public void Configure(EntityTypeBuilder<Author> builder)
+    {
+        builder.HasKey(a => a.Id);
+        
+    }
+}
