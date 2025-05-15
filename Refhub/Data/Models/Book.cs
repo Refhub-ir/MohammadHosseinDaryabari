eeba3a652792f7d33559b1f721b01532d0541d91
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 20:58:25 +0330

    add Book model

---
+++ Refhub/Data/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Refhub.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public int PageCount { get; set; }
+        public string FilePath { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public List<BookKeyword> BookKeywords { get; set; }
+        public List<BookAuthor> BookAuthors { get; set; }
+        public List<BookRelation> RelatedTo { get; set; }
+        public List<BookRelation> RelatedFrom { get; set; }
+    }
+}
