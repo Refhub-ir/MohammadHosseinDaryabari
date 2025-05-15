d012209c74105ef3554442dd41413c136a3d038c
Mammdoss <seyedmhdaryabari@gmail.com>
2025-05-15 21:00:45 +0330

    add DB Set to AppDBContext

--- Refhub/Data/Context/AppDBContext.cs
+++ Refhub/Data/Context/AppDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Models;
 using System;
 
 namespace Refhub.Data.Context;
@@ -7,4 +8,20 @@ public class AppDBContext : DbContext
 {
     public AppDBContext(DbContextOptions<AppDBContext> options)
         : base(options) { }
+
+    // DbSets
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Keyword> Keywords { get; set; }
+    public DbSet<BookAuthor> BookAuthors { get; set; }
+    public DbSet<BookKeyword> BookKeywords { get; set; }
+    public DbSet<BookRelation> BookRelations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppD‌BContext).Assembly);
+    }
 }
+
